144fd0e931d4f22f146c7297855bcd5795d1a45f
songusw <63530828+songusw@users.noreply.github.com>
2021-07-14 21:38:35 -0400

    some change

--- server/index.js
+++ server/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname,'../front-end/build')))
+app.use(express.static(path.resolve(__dirname, "../front-end/build")));
 
 app.use(express.json());
 //app.use(express.)
 
-app.post('/user', (req,res,next) => {
+app.post("/user", (req, res, next) => {});
 
-})
+app.get("*", (req, res, next) => {
+  res.sendFile(path.resolve(__dirname, "../front-end/build", "index.html"));
+});
 
-app.get('*',(req,res,next) => {
-    res.sendFile(path.resolve(__dirname, '../front-end/build' , 'index.html'))
-})
-
-app.listen(3000,()=> console.log("server is running"))
+mongoose.connect().then(() => {
+  app.listen(3000, () => console.log("server is running"));
+}).catch(err => {
+    console.log(err)
+});
